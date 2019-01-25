44b6fbfea1629cd532fbb8bcf91d283284e67835
Neill Webb <neill.webb@gmail.com>
2019-01-25 15:27:19 -0500

    single Page Application

---
+++ app.js
@@ -0,0 +1,35 @@
+function showUI1(){
+    document.getElementsByClassName("ui1").style.display="block";
+    document.getElementsByClassName("ui2").style.display="none";
+    document.getElementsByClassName("ui3").style.display="none";
+    document.getElementsByClassName("ui4").style.display="none";
+    document.getElementsByClassName("ui5").style.display="none";
+};
+function showUI2(){
+    document.getElementsByClassName("ui1").style.display="none";
+    document.getElementsByClassName("ui2").style.display="block";
+    document.getElementsByClassName("ui3").style.display="none";
+    document.getElementsByClassName("ui4").style.display="none";
+    document.getElementsByClassName("ui5").style.display="none";
+};
+function showUI3(){
+    document.getElementsByClassName("ui1").style.display="none";
+    document.getElementsByClassName("ui2").style.display="none";
+    document.getElementsByClassName("ui3").style.display="block";
+    document.getElementsByClassName("ui4").style.display="none";
+    document.getElementsByClassName("ui5").style.display="none";
+};
+function showUI4(){
+    document.getElementsByClassName("ui1").style.display="none";
+    document.getElementsByClassName("ui2").style.display="none";
+    document.getElementsByClassName("ui3").style.display="none";
+    document.getElementsByClassName("ui4").style.display="block";
+    document.getElementsByClassName("ui5").style.display="none";
+};
+function showUI5(){
+    document.getElementsByClassName("ui1").style.display="none";
+    document.getElementsByClassName("ui2").style.display="none";
+    document.getElementsByClassName("ui3").style.display="none";
+    document.getElementsByClassName("ui4").style.display="none";
+    document.getElementsByClassName("ui5").style.display="block";
+}
